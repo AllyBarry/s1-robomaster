59e1cf61938db41fe4382cf5ab0db0aee98d3729
Alexandra Barry <ali.barry3@gmail.com>
2026-03-16 16:41:04 +0200

    checkpoint

---
+++ copy_to_raspberry_pi.bash
@@ -0,0 +1,8 @@
+#!/bin/bash
+# declare -a arr=(2 3)
+# for i in "${arr[@]}"
+# 10.3.1.$i
+# do
+rsync -a --progress --exclude paper --exclude docs --exclude setup . rasppiuser@192.168.1.33:/home/rasppiuser/Robot
+# done
+
